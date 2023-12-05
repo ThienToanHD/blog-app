9fd137528846c7dbf2eeef52654c02f61a4d3194
ThienToanHD <114332213+ThienToanHD@users.noreply.github.com>
2023-12-05 22:17:12 +0900

    [Add] Find all post API

--- src/main/java/com/vti/blogapp/service/PostServiceImpl.java
+++ src/main/java/com/vti/blogapp/service/PostServiceImpl.java
@@ -6,12 +6,24 @@ import com.vti.blogapp.form.PostCreateForm;
 import com.vti.blogapp.mapper.PostMapper;
 import com.vti.blogapp.repository.PostRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class PostServiceImpl implements PostService{
     private final PostRepository postRepository;
+
+    @Override
+    public Page<PostDto> findAll(Pageable pageable) {
+        // lambda: Tìm hiểu
+        // method reference: Tìm hiểu
+       return postRepository.findAll(pageable).map(PostMapper::map);
+    }
 
     @Override
     public PostDto create(PostCreateForm form) {
